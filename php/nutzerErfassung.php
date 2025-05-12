8a74942780488f9bd0e45e7874f6b42c9dc23038
JanUrech <janic.urech@stud.fhgr.ch>
2025-05-12 14:40:02 +0200

    Aufräumen der alten dateien von den Unterrichts èbungen, dann habe ich Das formular so umgebaut, dass die E-Mail nur ein mal vorkommen kann und das Passwort enkryptet wird.

--- php/nutzerErfassung.php
+++ php/nutzerErfassung.php
@@ -7,12 +7,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Eingaben aus dem Formular
     $vorname     = $_POST["vorname"];
     $nachname    = $_POST["nachname"];
+    $email = $_POST["email"];
     $password    = $_POST["password"];
     $alter_jahre       = $_POST["alter_jahre"];
     $geschlecht  = $_POST["geschlecht"];
     $groesse_cm  = $_POST["groesse_cm"];
     $gewicht_kg  = $_POST["gewicht_kg"];
 
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// check if email already exists
+$stmt = $pdo->prepare("SELECT COUNT(*) FROM nutzer WHERE email = :email");
+$stmt->execute([ ':email' => $email ]);
+$user = $stmt->fetchColumn(); // korrekt mit $
+
+if ($user > 0) {
+    echo "❗️Ein Benutzer mit dieser E-Mail existiert bereits.";
+    exit;
+}
+ 
     // BMI berechnen
     if ($groesse_cm > 0) {
         $bmi = round($gewicht_kg / pow($groesse_cm / 100, 2), 1);
@@ -21,15 +34,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // SQL vorbereiten
-    $sql = "INSERT INTO nutzer (vorname, nachname, password, alter_jahre, geschlecht, groesse_cm, gewicht_kg, BMI)
-            VALUES (:vorname, :nachname, :password, :alter_jahre, :geschlecht, :groesse_cm, :gewicht_kg, :bmi)";
+    $sql = "INSERT INTO nutzer (vorname, nachname, email, password, alter_jahre, geschlecht, groesse_cm, gewicht_kg, BMI)
+    VALUES (:vorname, :nachname, :email, :password, :alter_jahre, :geschlecht, :groesse_cm, :gewicht_kg, :bmi)";
 
     try {
         $stmt = $pdo->prepare($sql);
         $stmt->execute([
             ':vorname'     => $vorname,
             ':nachname'    => $nachname,
-            ':password'    => $password,
+            ':email'       => $email,
+            ':password'    => $hashed_password,
             ':alter_jahre'       => $alter_jahre,
             ':geschlecht'  => $geschlecht,
             ':groesse_cm'  => $groesse_cm,
